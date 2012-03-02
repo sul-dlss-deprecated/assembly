e063117a73b249264dc49161667777e7d572c8e5
Monty Hindman <montyhindman@gmail.com>
2012-03-01 19:02:57 -0800

    Removed old TODOs.

--- config/boot.rb
+++ config/boot.rb
@@ -11,8 +11,7 @@ ROBOT_ROOT = File.expand_path(File.dirname(__FILE__) + "/..")
 ROBOT_LOG = Logger.new(File.join(ROBOT_ROOT, "log/#{environment}.log"))
 ROBOT_LOG.level = Logger::SEV_LABEL.index(ENV['ROBOT_LOG_LEVEL']) || Logger::INFO
 
-# Override Solrizer's logger before it gets a chance to load and pollute
-# STDERR.
+# Override Solrizer's logger before it gets a chance to load and pollute STDERR.
 begin
   require 'solrizer'
   Solrizer.logger = ROBOT_LOG
@@ -22,7 +21,6 @@ end
 require 'dor-services'
 require 'lyber_core'
 
-# TODO Maybe move auto-require to just run_robot and spec_helper?
 Dir["#{ROBOT_ROOT}/lib/**/*.rb"].each { |f| require f }
 Dir["#{ROBOT_ROOT}/robots/*"].select { |f| File.directory?(f) }.each do |dir|
   module_name = File.basename(dir).split(/_/).map {|s| s.capitalize}.join('')
