bb9f3dd592e7b58e3ce9280e61d2681c3d16527f
Monty Hindman <montyhindman@gmail.com>
2012-03-06 09:10:56 -0800

    Wrapped up item.rb and its specs.

--- lib/item.rb
+++ lib/item.rb
@@ -5,8 +5,6 @@ module Dor::Assembly
     include Dor::Assembly::Checksumable
     include Dor::Assembly::ContentMetadata
 
-    attr_accessor :druid, :root_dir, :cm_file_name
-
     def initialize(params = {})
       @druid = params[:druid]
       setup
@@ -14,7 +12,6 @@ module Dor::Assembly
     end
 
     def setup
-      # TODO: setup(): @cm_file_type does not belong here.
       @druid        = Druid.new(@druid) unless @druid.class == Druid
       @root_dir     = Dor::Config.assembly.root
       @cm_file_name = File.join druid_tree_path, 'content_metadata.xml'
@@ -23,4 +20,3 @@ module Dor::Assembly
   end
   
 end
-
--- spec/lib/item_spec.rb
+++ spec/lib/item_spec.rb
@@ -1,34 +1,25 @@
 describe Dor::Assembly::Item do
   
   before :each do
-    @dru           = 'aa111bb2222'
-    @druid         = Druid.new @dru
-    @root_dir      = Dor::Config.assembly.root
-    @ai            = new_assembly_item @druid
+    @dru      = 'aa111bb2222'
+    @druid    = Druid.new @dru
+    @root_dir = 'spec/test_input'
   end
 
-  def new_assembly_item(druid)
-    # TODO: new_assembly_item: use a StringIO for cm_handle.
-    @ai = Dor::Assembly::Item.new(
-      :druid     => druid,
-      :cm_handle => File.open('tmp/out.xml', 'a')
-    )
-  end
- 
   describe "initialization" do
     
-    it "can be instantiated" do
-      @ai.should be_kind_of Dor::Assembly::Item
-    end
-    
     it "knows its druid, whether passed a string druid or a Druid object" do
-      @ai.druid.should == @druid
-      @ai = new_assembly_item @dru
-      @ai.druid.druid.should == @dru
+      @item = Dor::Assembly::Item.new :druid => @druid
+      @item.druid.should == @druid
+
+      @item = Dor::Assembly::Item.new :druid => @druid
+      @item.druid.druid.should == @dru
     end
 
-    it "knows its druid_tree_path" do
-      @ai.druid_tree_path.should == File.join(@root_dir, @druid.tree)
+    it "knows the path to the content metadata file" do
+      @item = Dor::Assembly::Item.new :druid => @druid
+      exp_cm_file = 'spec/test_input/aa/111/bb/2222/content_metadata.xml'
+      @item.cm_file_name.should == exp_cm_file
     end
 
   end
