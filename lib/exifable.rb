5521e7b38657e4b02bd02177cece67ccb1d9128b
Monty Hindman <montyhindman@gmail.com>
2012-03-13 15:19:09 -0700

    Wrote specs for simple XML methods in exifable.rb.

--- lib/exifable.rb
+++ lib/exifable.rb
@@ -3,64 +3,29 @@ module Dor::Assembly
     
     include Dor::Assembly::ContentMetadata
 
+    ATTR_XML = '<attr name="representation">uncropped</attr>'
+
     def collect_exif_info
-
-      # for fn,img in node-images
-      #   next unless [fn,img] is relevant (tiff, jpeg, jp2)
-      #   e = img.exif
-      #   modify_cm(fn,e) 
-
-      # For each supported image type, generate a jp2 derivative
-      # and modify content metadata XML to reflect the new file.
-      # relevant_images.each do |fn, img|
-      #   jp2       = img.create_jp2
-      #   file_name = File.basename jp2.path
-      #   add_jp2_file_node fn.parent, file_name
-      # end
+      relevant_fnode_image_tuples(:tif, :jpg, :jp2).each do |fn, img|
+        set_node_type_as_image fn.parent
+        fn.add_child image_data_xml(img.exif)
+        fn.add_child ATTR_XML
+      end
+      set_node_type_as_image @cm.root
 
       # Save the modified XML.
       # persist_content_metadata
     end
 
-    # def all_images
-    #   # Returns a list of node-Image pairs.
-    #   file_nodes.map { |fn| [ fn, Assembly::Image.new(path_to_file fn['id']) ] }
-    # end
+    def set_node_type_as_image(node)
+      node['type'] = 'image'
+    end
 
-    # def relevant_images
-    #   # Returns a list of node-Image pairs,  after filtering out unsupported types.
-    #   approved = ['image/tiff', 'image/jpeg']
-    #   all_images.select { |fn, img| approved.include? img.exif.mimeType }
-    # end
-
-    # def add_jp2_file_node(parent_node, file_name)
-    #   # Adds a file node representing the new jp2 file.
-    #   f = %Q(<file preserve="yes" publish="no" shelve="no" id="#{file_name}" />)
-    #   parent_node.add_child f
-    # end
-
-    # <contentMetadata type="image">
-    #   <resource type="image">
-    #     <file mimetype="image/tiff">
-    #       <imageData width="100" height="100"/>
-    #       <attr name="representation">uncropped</attr>
-    #     </file>
-    #     <file mimetype="image/jp2">
-    #       <imageData width="100" height="100"/>
-    #       <attr name="representation">uncropped</attr>
-    #     </file>
-    #   </resource>
-    #   <resource type="image">
-    #     <file mimetype="image/tiff">
-    #       <imageData width="100" height="100"/>
-    #       <attr name="representation">uncropped</attr>
-    #     </file>
-    #     <file mimetype="image/jp2">
-    #       <imageData width="100" height="100"/>
-    #       <attr name="representation">uncropped</attr>
-    #     </file>
-    #   </resource>
-    # </contentMetadata>
+    def image_data_xml(exif)
+      w = exif.image_width
+      h = exif.image_height
+      %Q(<imageData width="#{w}" height="#{h}"/>)
+    end
 
   end
 end
--- spec/lib/exifable_spec.rb
+++ spec/lib/exifable_spec.rb
@@ -18,106 +18,53 @@ describe Dor::Assembly::Exifable do
     @item.cm_file_name = File.join root_dir, @item.druid.path, cm_file_name
     @dummy_xml         = '<contentMetadata><resource></resource></contentMetadata>'
   end
- 
+
   describe '#ExifableItem' do
     it 'should be able to initialize our testing object' do
       @item.should be_a_kind_of ExifableItem
     end
   end
 
-  # describe '#create_jp2s' do
-  # 
-  #   before(:each) do
-  #     # Use rsync to create a copy of the test_input directory that we can modify.
-  #     @tmp_root_dir = "tmp/test_input"
-  #     root_dir = Dor::Config.assembly.root_dir
-  #     system "rsync -rqOlt --delete #{root_dir}/ #{@tmp_root_dir}/"
-  #   end
+  describe 'Simple XML methods' do
+  
+    it '#set_node_type_as_image should add type="image" attributes correctly' do
+      @item.cm = noko_doc @dummy_xml
+      %w(contentMetadata resource).each do |tag|
+          node = @item.cm.xpath("//#{tag}").first
+          @item.set_node_type_as_image node
+      end
+      exp = Nokogiri::XML( '<contentMetadata type="image"><resource type="image">' + 
+                           '</resource></contentMetadata>')
+      @item.cm.should be_equivalent_to exp
+    end
 
-  #   it 'should create the expected jp2 files' do
-  #     basic_setup 'aa111bb2222', @tmp_root_dir
-  #     @item.load_content_metadata
-  #     tifs = @item.file_nodes.map { |fn| @item.path_to_file fn['id'] }
-  #     jp2s = tifs.map { |t| t.sub /\.tif$/, '.jp2' }
+    it '#image_data_xml should return the expect XML text' do
+      exif = double 'image_width' => 55, 'image_height' => 66
+      @item.image_data_xml(exif).should == '<imageData width="55" height="66"/>'
+    end
 
-  #     # Only tifs should exist.
-  #     @item.file_nodes.size.should == 2
-  #     tifs.all?  { |t| File.file? t }.should == true
-  #     jp2s.none? { |j| File.file? j }.should == true
+  end
 
-  #     # Both tifs and jp2s should exist.
-  #     @item.create_jp2s
-  #     @item.file_nodes.size.should == 4
-  #     tifs.all? { |t| File.file? t }.should == true
-  #     jp2s.all? { |j| File.file? j }.should == true
-  #   end
+  describe '#collect_exif_info' do
+  
+    # before(:each) do
+    #   # Use rsync to create a copy of the test_input directory that we can modify.
+    #   @tmp_root_dir = "tmp/test_input"
+    #   root_dir = Dor::Config.assembly.root_dir
+    #   system "rsync -rqOlt --delete #{root_dir}/ #{@tmp_root_dir}/"
+    # end
 
-  #   it 'should persist the expected changes to content metadata XML file' do
-  #     basic_setup 'aa111bb2222', @tmp_root_dir
-  #     @item.load_content_metadata
+    # it 'should persist the expected changes to content metadata XML file' do
+    #   basic_setup 'aa111bb2222', @tmp_root_dir
+    #   @item.load_content_metadata
+    #   @item.collect_exif_info
 
-  #     # Only 2 tifs should exist before calling method.
-  #     @item.file_nodes.size.should == 2
-  #     @item.create_jp2s
+    #   # Read the XML file and check a few things.
+    #   xml = Nokogiri::XML File.read(@item.cm_file_name)
+    #   file_nodes = xml.xpath "//resource/file"
+    #   file_nodes.map { |fn| fn['id'] }
+    # end
 
-  #     # Read the XML file and check the file names.
-  #     xml = Nokogiri::XML File.read(@item.cm_file_name)
-  #     file_nodes = xml.xpath "//resource/file"
-  #     file_nodes.map { |fn| fn['id'] }.sort.should == %w(
-  #       image111.jp2 image111.tif
-  #       image112.jp2 image112.tif
-  #     )
-  #   end
-
-  # end
-
-  # describe "Filters" do
-  #   
-  #   it "#all_images should load the correct N of Node-Image pairs" do
-  #     basic_setup 'aa111bb2222'
-  #     @item.load_content_metadata
-  #     imgs = @item.all_images
-  #     imgs.size.should == 2
-  #     imgs.each { |file_node, img|
-  #       file_node.should be_instance_of Nokogiri::XML::Element
-  #       img.should be_instance_of Assembly::Image
-  #     }
-
-  #     basic_setup 'cc333dd4444'
-  #     @item.load_content_metadata
-  #     imgs = @item.all_images
-  #     imgs.size.should == 2
-  #   end
-
-  #   it "#relevant_images should filter out non-approved file types" do
-  #     basic_setup 'cc333dd4444'
-  #     @item.load_content_metadata
-  #     imgs = @item.relevant_images
-  #     imgs.size.should == 1
-  #   end
-
-  # end
-
-  # describe '#add_jp2_file_node' do
-  #   
-  #   it 'should add the expected <file> node to XML' do
-  #     exp_xml = <<-END.gsub(/^ {8}/, '')
-  #       <?xml version="1.0"?>
-  #       <contentMetadata>
-  #         <resource>
-  #           <file preserve="yes" publish="no" shelve="no" id="foo.tif"/>
-  #         </resource>
-  #       </contentMetadata>
-  #     END
-  #     exp_xml = noko_doc exp_xml
-
-  #     @item.cm = noko_doc @dummy_xml
-  #     resource_node = @item.cm.xpath('//resource').first
-
-  #     @item.add_jp2_file_node resource_node, 'foo.tif'
-  #     @item.cm.should be_equivalent_to exp_xml
-  #   end
-
-  # end
+  end
 
 end
