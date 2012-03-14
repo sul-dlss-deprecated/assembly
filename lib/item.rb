63df36ba811c1a851525072a5bdea94bb0d33a38
Monty Hindman <montyhindman@gmail.com>
2012-03-14 16:24:08 -0700

    Added missing includes in Assembly::Item.

--- lib/item.rb
+++ lib/item.rb
@@ -3,8 +3,12 @@ module Dor::Assembly
   class Item
 
     include Dor::Assembly::ContentMetadata
+
+    include Dor::Assembly::Jp2able
     include Dor::Assembly::Checksumable
     include Dor::Assembly::ChecksumComparable
+    include Dor::Assembly::Exifable
+    include Dor::Assembly::Accessionable
 
     def initialize(params = {})
       # Takes a druid, either as a string or as a Druid object.
