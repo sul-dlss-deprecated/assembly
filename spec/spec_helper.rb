2510165e21fab36e00a06973e75c07289d139627
Peter Mangiafico <peter@mangiafico.org>
2012-03-14 11:08:57 -0700

    updated README and create tmp directory if needed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ bootfile    = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 require bootfile
 require 'tempfile'  
 require 'equivalent-xml'  
+
+tmp_output_dir = File.join(ROBOT_ROOT, 'tmp')
+FileUtils.mkdir_p tmp_output_dir
   
 def noko_doc(x)
   # Returns Nokogiri XML Document, with config to ignore blanks.
