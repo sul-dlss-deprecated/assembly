2bd895293d29d247ab1aa474e7f78a772c515e01
Peter Mangiafico <peter@mangiafico.org>
2012-03-15 10:52:00 -0700

    monkey patch for SSL self-signing cert on lyberservices-dev

--- lib/accessionable.rb
+++ lib/accessionable.rb
@@ -1,3 +1,18 @@
+# monkey patch to fix SSL problem on sul-lyberservices-dev 
+# TODO fix me so we don't need to monkey patch the dor services gem
+module Dor
+  module WorkflowService
+    class << self
+      def workflow_resource
+        RestClient::Resource.new(Config.workflow.url,
+        :ssl_client_cert  =>  OpenSSL::X509::Certificate.new(File.read(Config.fedora.cert_file)),
+        :ssl_client_key   =>  OpenSSL::PKey::RSA.new(File.read(Config.fedora.key_file), Config.fedora.key_pass)),
+        :verify_ssl => OpenSSL::SSL::VERIFY_NONE
+      end
+    end
+  end
+end
+
 module Dor::Assembly
   module Accessionable
     
