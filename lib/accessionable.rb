5cc090b63b1b7f32f39b9650618c133e4cc23112
Monty Hindman <montyhindman@gmail.com>
2012-03-30 10:34:24 -0700

    Minor code cleanup.

--- lib/accessionable.rb
+++ lib/accessionable.rb
@@ -6,19 +6,19 @@ module Dor::Assembly
     include Dor::Assembly::ContentMetadata
 
     def initiate_accessioning
-      
       initialize_workspace
       initialize_apo_workflow
-      
     end
     
     def initialize_workspace
-      source_path=@druid.path(Dor::Config.assembly.root_dir)
-      RestClient.post "#{Dor::Config.dor.service_root}/dor/v1/objects/druid:#{@druid.id}/initialize_workspace",:source=>source_path      
+      source_path = @druid.path(Dor::Config.assembly.root_dir)
+      url         = "#{Dor::Config.dor.service_root}/dor/v1/objects/druid:#{@druid.id}/initialize_workspace"
+      RestClient.post url, :source => source_path      
     end
     
     def initialize_apo_workflow
-      RestClient.post "#{Dor::Config.dor.service_root}/dor/v1/objects/druid:#{@druid.id}/accession",{}       
+      url = "#{Dor::Config.dor.service_root}/dor/v1/objects/druid:#{@druid.id}/accession"
+      RestClient.post url, {}       
     end
     
   end
