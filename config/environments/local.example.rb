3de4d9e9823ae59b9ece48db342acf77d8b58939
Peter Mangiafico <peter@mangiafico.org>
2012-07-13 10:13:29 -0700

    add example config file

---
+++ config/environments/local.example.rb
@@ -0,0 +1,32 @@
+CERT_DIR = File.join(File.dirname(__FILE__), "..", "certs")
+
+Dor::Config.configure do
+
+  fedora do
+    url       'https://USERNAME:PASSWORD@dor-dev.stanford.edu/fedora'
+  end
+
+  ssl do
+    cert_file File.join(CERT_DIR, "dlss-dev-test.crt")
+    key_file  File.join(CERT_DIR, "dlss-dev-test.key")
+    key_pass  ''
+  end
+  
+  workflow.url 'https://lyberservices-dev.stanford.edu/workflow/'
+
+  robots do 
+    workspace '/tmp'
+  end
+
+  assembly do
+    root_dir      'spec/test_input'
+    cm_file_name  'contentMetadata.xml'
+    dm_file_name  'descMetadata.xml'
+    next_workflow 'accessionWF'
+  end
+
+  dor do
+    service_root 'https://USERNAME:PASSWORD@lyberservices-dev.stanford.edu'
+  end
+
+end
