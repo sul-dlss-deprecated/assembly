85062369108886f7393d281b6e2a71fafe9025ba
Peter Mangiafico <peter@mangiafico.org>
2012-11-21 21:22:25 -0800

    update documentation

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -19,13 +19,13 @@ Dor::Config.configure do
   end
 
   assembly do
-    root_dir      ['spec/test_input','spec/test_input2']
-    cm_file_name  'contentMetadata.xml'
-    dm_file_name  'descMetadata.xml'
-    next_workflow 'accessionWF'
-    overwrite_jp2     false
-    overwrite_dpg_jp2 false
-    robot_sleep_time 30
+    root_dir      ['spec/test_input','spec/test_input2'] # directories to search for content that should be acted upon by the robots
+    cm_file_name  'contentMetadata.xml' # the name of the contentMetadata file
+    dm_file_name  'descMetadata.xml' # the name of the descMetadata file
+    next_workflow 'accessionWF' # name of the next workflow to start after assembly robots are done
+    overwrite_jp2     false # indicates if the jp2-create robot should overwrite an existing jp2 of the same name as the new one being created
+    overwrite_dpg_jp2 false # indicates if the jp2-create robot should create a jp2 when there is a corresponding DPG style jp2 (e.g. oo000oo0001_00_001.tif and oo000oo0001_05_001.jp2, then a "false" setting here would NOT generate a new jp2 even though there is no filename clash)
+    robot_sleep_time 30     # how long robots will sleep before attemping to connect to workflow service again
   end
 
   dor do
