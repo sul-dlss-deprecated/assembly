f50b1003fd1b2fecd543a847429e336607c660c4
Ahmed AlSum <aalsum@stanford.edu>
2014-04-28 14:28:04 -0700

    Modifying the robots code to reflect the new framework

--- robots/assembly/exif_collect.rb
+++ robots/assembly/exif_collect.rb
@@ -1,21 +1,26 @@
-module Assembly
+module Robots
+  module DorRepo
+    module Assembly
   
-  class ExifCollect < LyberCore::Robots::Robot
+      class ExifCollect 
+        
+        include LyberCore::Robot
+        
+        def initialize(opts = {})
+          super('assemblyWF', 'exif-collect', opts)
+        end
 
-    def initialize(opts = {})
-      super('assemblyWF', 'exif-collect', opts)
-    end
+        def process_item(work_item)
+          ai = Dor::Assembly::Item.new :druid => work_item.druid
+          if (Dor::Config.configure.assembly.items_only && !ai.is_item?) 
+             Assembly::ExifCollect.logger.warn("Skipping exif-collect for #{work_item.druid} since it is not an item")
+          else
+            ai.load_content_metadata
+            ai.collect_exif_info
+          end
+        end
 
-    def process_item(work_item)
-      ai = Dor::Assembly::Item.new :druid => work_item.druid
-      if (Dor::Config.configure.assembly.items_only && !ai.is_item?) 
-         Assembly::ExifCollect.logger.warn("Skipping exif-collect for #{work_item.druid} since it is not an item")
-      else
-        ai.load_content_metadata
-        ai.collect_exif_info
       end
     end
-
   end
-
 end
