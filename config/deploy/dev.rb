462c1fbc19a305b6b3c1389a145ea804e815b384
Willy Mene <wmene@stanford.edu>
2014-05-01 10:17:01 -0700

    lyberservices-dev deploy

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,8 +1,7 @@
-#server 'sul-lyberservices-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots1-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots2-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-lyberservices-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+#server 'sul-robots1-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+#server 'sul-robots2-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
-set :branch, 'assembly-aalsum'
-set :deploy_environment, 'development'
+set :branch, 'dev-deploy'
