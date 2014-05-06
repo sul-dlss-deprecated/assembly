c76709996ca124d2eaf4df530ff02636b28ca5f2
Willy Mene <wmene@stanford.edu>
2014-05-06 16:17:56 -0700

    Add run as shared directory for cap deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :deploy_to, "/home/lyberadmin/#{fetch(:application)}"
 
 set :stages, %W(dev staging production)
 
-set :linked_dirs, %w(log config/environments config/certs)
+set :linked_dirs, %w(log run config/environments config/certs)
 
 namespace :deploy do
 
