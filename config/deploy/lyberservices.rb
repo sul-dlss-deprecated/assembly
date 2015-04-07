c1f4ccef5551067dd73a73e571c285e0f3d62e4a
Peter Mangiafico <peter@mangiafico.org>
2015-04-07 10:52:49 -0700

    fix more issues with deployment

--- config/deploy/lyberservices.rb
+++ config/deploy/lyberservices.rb
@@ -1,18 +1,7 @@
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    puts 'skipping'
-  end
-
-  after :publishing, :restart
-
-end
-
 server 'sul-lyberservices-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :branch, 'master'
-set :deploy_environment, 'lyberservices'
-set :default_env, { :robot_environment => 'production' }
+set :deploy_environment, 'production'
+set :default_env, { :robot_environment => fetch(:deploy_environment) }
