377a5f543331a49d1368c365238061ab4e9f39ba
Chris Beer <cabeer@stanford.edu>
2016-02-16 11:56:48 -0800

    Deploy master to the -dev environment

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -6,5 +6,4 @@ server 'sul-robots5-dev.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
-set :branch, 'dev-deploy'
 set :default_env, { :robot_environment => fetch(:deploy_environment) }
