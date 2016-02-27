f0f26875ed265a797969ef8117061728d28c3db7
Darren Hardy <drh@stanford.edu>
2016-02-26 16:53:42 -0800

    remove old VMs

--- config/deploy/lyberservices.rb
+++
@@ -1,7 +0,0 @@
-server 'sul-lyberservices-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
-set :branch, 'master'
-set :deploy_environment, 'production'
-set :default_env, { :robot_environment => fetch(:deploy_environment) }
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,5 @@
 server 'sul-robots1-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 server 'sul-robots2-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots3-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots4-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots5-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
