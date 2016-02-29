1566421eba0aaa765c35e54326957c0cb7618b4c
Darren Hardy <drh@stanford.edu>
2016-02-29 12:56:08 -0800

    add whenever environment variables

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,4 +4,6 @@ server 'sul-robots2-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :deploy_environment, 'test'
+set :whenever_environment, fetch(:deploy_environment)
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:deploy_environment)}" }
 set :default_env, { :robot_environment => fetch(:deploy_environment) }
