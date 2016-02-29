97c2498103d84a3827aef0d490bd2a056f12603f
Darren Hardy <drh@stanford.edu>
2016-02-29 12:10:08 -0800

    ask for the branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'assembly'
 set :repo_url, 'https://github.com/sul-dlss/assembly'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, "/home/lyberadmin/#{fetch(:application)}"
