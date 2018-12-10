c8fce4725c8258d398f7f3ac28a421d0ac30ee53
Justin Coyne <jcoyne@justincoyne.com>
2018-12-10 13:40:29 -0600

    Move business logic off the pseudo-model and directly into the robot class

--- spec/robots/assembly/accessioning_initiate_spec.rb
+++ spec/robots/assembly/accessioning_initiate_spec.rb
@@ -1,44 +1,65 @@
 require 'spec_helper'
 
-describe Robots::DorRepo::Assembly::AccessioningInitiate do
-  before :each do
-    @druid = 'aa222cc3333'
-    allow(RestClient).to receive(:post) # don't actually make the RestClient calls, just assume they work
+RSpec.describe Robots::DorRepo::Assembly::AccessioningInitiate do
+  let(:druid) { 'aa222cc3333' }
+
+  before do
     allow(Dor::Assembly::Item).to receive(:new).and_return(@assembly_item)
-    @r = Robots::DorRepo::Assembly::AccessioningInitiate.new(:druid => @druid)
+    @r = Robots::DorRepo::Assembly::AccessioningInitiate.new(:druid => druid)
   end
 
-  it "should initiate accessioning for type=item" do
-    setup_assembly_item(@druid, :item)
-    expect(@assembly_item.is_item?).to be_truthy
-    expect(@assembly_item).to receive(:initialize_workspace)
-    expect(@assembly_item).to receive(:initialize_workflow)
-    @r.perform(@assembly_item)
+  context 'when the type is item' do
+    before do
+      setup_assembly_item(druid, :item)
+      stub_request(:post, "https://localhost/dor/v1/objects/druid:aa222cc3333/initialize_workspace")
+        .with(body: { "source" => nil })
+        .to_return(status: 200, body: "")
+      stub_request(:post, "https://localhost/dor/v1/objects/druid:aa222cc3333/apo_workflows/accessionWF")
+        .to_return(status: 200, body: "")
+    end
+
+    it "initiates accessioning" do
+      expect(@assembly_item.is_item?).to be_truthy
+      @r.perform(@assembly_item)
+    end
   end
 
-  it "should initiate accessioning for type=set but not initalize the workspace" do
-    setup_assembly_item(@druid, :set)
-    expect(@assembly_item.is_item?).to be_falsey
-    expect(@assembly_item).to_not receive(:initialize_workspace)
-    expect(@assembly_item).to receive(:initialize_workflow)
-    @r.perform(@assembly_item)
-  end
+  context 'when the type is set' do
+    before do
+      setup_assembly_item(druid, :set)
+    end
 
-  it "should always initiate accessioning regardless of object type and configuration of items_only=false but not initalize the workspace" do
-    Dor::Config.configure.assembly.items_only = false
-    setup_assembly_item(@druid, :set)
-    expect(@assembly_item.is_item?).to be_falsey
-    expect(@assembly_item).to_not receive(:initialize_workspace)
-    expect(@assembly_item).to receive(:initialize_workflow)
-    @r.perform(@assembly_item)
-  end
+    context 'and items_only is the default' do
+      it "initiates accessioning, but does not initialize the workspace" do
+        expect(@assembly_item.is_item?).to be_falsey
+        expect(@r).to_not receive(:initialize_workspace)
+        expect(@r).to receive(:initialize_workflow)
+        @r.perform(@assembly_item)
+      end
+    end
 
-  it "should always initiate accessioning regardless of object type and configuration of items_only=true but not initalize the workspace" do
-    Dor::Config.configure.assembly.items_only = true
-    setup_assembly_item(@druid, :set)
-    expect(@assembly_item.is_item?).to be_falsey
-    expect(@assembly_item).to_not receive(:initialize_workspace)
-    expect(@assembly_item).to receive(:initialize_workflow)
-    @r.perform(@assembly_item)
+    context 'and items_only is set to false' do
+      before do
+        Dor::Config.configure.assembly.items_only = false
+      end
+      it "initiates accessioning, but does not initialize the workspace" do
+        expect(@assembly_item.is_item?).to be_falsey
+        expect(@r).to_not receive(:initialize_workspace)
+        expect(@r).to receive(:initialize_workflow)
+        @r.perform(@assembly_item)
+      end
+    end
+
+    context 'and items_only is set to true' do
+      before do
+        Dor::Config.configure.assembly.items_only = true
+      end
+      it "initiates accessioning, but does not initialize the workspace" do
+        expect(@assembly_item.is_item?).to be_falsey
+        expect(@r).to_not receive(:initialize_workspace)
+        expect(@r).to receive(:initialize_workflow)
+        @r.perform(@assembly_item)
+      end
+    end
   end
 end
